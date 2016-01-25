080943ce795cea6f8b1448078935d12d6c7238aa
Chandra Sekhar <chandu2chandu1@gmail.com>
2016-01-25 21:56:45 +0530

    Updated with some basic elements

--- AngularBasics/Customers/CustomerModule.js
+++ AngularBasics/Customers/CustomerModule.js
@@ -3,7 +3,33 @@ var app = new angular.module('custModule',[]);
 app.controller('custController', function () {
     var customerCtrl = this;
 
-    customerCtrl.customerList = [];
+    //Initialize with dummy data
+    customerCtrl.customerList = [
+           {
+               "firstName": "Chandra",
+               "lastName": "Sekhar",
+               "country": "India",
+               "gender":"Male",
+               "email": "chandu2chandu1@gmail.com",
+               "dateOfBirth": "2016-01-30T18:30:00.000Z"
+           },
+           {
+               "firstName": "Lakshith",
+               "lastName": "CS",
+               "dateOfBirth": "2016-01-27T18:30:00.000Z",
+               "gender": "Male",
+               "country": "USA",
+               "email": "slakshithcs@gmail.com"
+           },
+           {
+               "firstName": "Amitabh",
+               "lastName": "Bacchan",
+               "dateOfBirth": "1963-10-08T18:30:00.000Z",
+               "country": "India",
+               "gender": "Male",
+               "email": "amitabh@bacchan.com"
+           }
+        ];
 
     customerCtrl.customer = {};
 
