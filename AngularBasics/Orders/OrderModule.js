a6b32e0a270c006c5245b6825cd49d9cbd84a4e5
Chandra Sekhar <chandu2chandu1@gmail.com>
2016-02-03 22:34:05 +0530

    Added a new Order Module

---
+++ AngularBasics/Orders/OrderModule.js
@@ -0,0 +1,21 @@
+﻿/// <reference path="../Scripts/angular.js" />
+
+var app = new angular.module('ordModule', []);
+
+app.controller("orderController", [function ($http) {
+    //this.Orders = [
+    //{
+    //    id: 1,
+    //    name: "Chandra"
+    //}
+    //];
+    this.Orders = function () {
+        $http({
+            method: 'GET',
+            url: 'http://services.odata.org/V4/Northwind/Northwind.svc/Orders'
+        }).then(function successCallback(response) {
+            alert("Response loaded");
+            console.log(response);
+        })
+    };
+}]);
