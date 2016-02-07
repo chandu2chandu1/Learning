4a92b407536efc286684161f1a3bbb62e93d755d
Chandra Sekhar <chandu2chandu1@gmail.com>
2016-02-07 10:02:46 +0530

    Added few files

--- AngularBasics/Orders/OrderModule.js
+++ AngularBasics/Orders/OrderModule.js
@@ -4,13 +4,24 @@ var app = new angular.module('ordModule', []);
 
 app.controller("orderController", ["$http", function ($http) {
     var orderCtrl = this;
+    orderCtrl.isLoaded = true;
     orderCtrl.getOrders = function () {
+        orderCtrl.isLoaded = false;
         $http({
             method: 'GET',
-            url: 'http://services.odata.org/V4/Northwind/Northwind.svc/Orders'
+            url: 'http://services.odata.org/V4/Northwind/Northwind.svc/Orders?$top=20'
         }).then(function successCallback(response) {
+            orderCtrl.isLoaded = true;
             orderCtrl.orderList = response.data.value;
             console.log(response);
         })
+    };
+
+    orderCtrl.clearOrders = function () {
+        orderCtrl.orderList = {};
+    };
+
+    orderCtrl.getOrder = function (currentOrder) {
+        orderCtrl.currentOrder = currentOrder;
     };
 }]);
